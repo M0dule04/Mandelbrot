e4739f8fea4541fb70faa7adfc3abc456afe39bd
M00dle <derich.havennga@gmail.com>
2023-01-11 15:39:45 +0200

    Added Window Procedure

--- Mandelbrot/Main.cpp
+++ Mandelbrot/Main.cpp
@@ -7,21 +7,25 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
 	WCHAR classname[] = L"Mandelbrot";
-	Control* frame = new Control(hInstance,classname);
+	Control<Form>* frame = new Control<Form>(hInstance,classname);
 
 	frame->setWindowName(classname);
 	frame->setHeight(500);
 	frame->setWidth(500);
 	frame->setX(15);
 	frame->setY(15);
-	frame->setStyles(WS_VISIBLE || WS_MINIMIZEBOX);
+	frame->setStyles(WS_TILEDWINDOW);
 
 	if (!frame->Create(NULL, NULL, NULL))
 	{
 		return 0;
 	}
 
-	//frame->Show(SW_NORMAL);
+	MSG msg = {};
+	while (GetMessage(&msg,NULL, 0,0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
 	return 0;
 }
--- Mandelbrot/WindowClasses.h
+++ Mandelbrot/WindowClasses.h
@@ -1,8 +1,23 @@
 #pragma once
 #include <windows.h>
 
+template <class DERIVED> 
 class Control // base window class
 {
+protected:
+	virtual LRESULT MessageHandler(HWND hWND, UINT uMsg, WPARAM wParam,  LPARAM lParam)
+	{
+		switch (uMsg)
+		{
+		case WM_CLOSE:
+			PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hWND, uMsg, wParam, lParam);
+			break;
+		}
+	}
+
 	private:
 		WNDCLASS wc = {};
 		WCHAR *ClassName; //Name used to reference window
@@ -18,44 +33,203 @@ class Control // base window class
 		int y = CW_USEDEFAULT; // Y position of window relatiive to parent
 		int width = CW_USEDEFAULT; // width of the window
 		int height = CW_USEDEFAULT; // height of the window
-		void Move();
-		void MessageHandler();
+
+		void Move()
+		{
+			if (hWND != NULL)
+			{
+				MoveWindow(hWND, x, y, width, height, TRUE);
+			}
+		}
+
 	public: 
-		int getX(); // returbs the x position of the window
-		int getY(); // returns the y position of the window
-		void setX(int x); // sets the X position of the window
-		void setY(int y); // sets the Y position of the window
+		static LRESULT CALLBACK WindowProc(HWND hWND, UINT uMessage, WPARAM wParam, LPARAM lParam) {
+
+			DERIVED* pClass = NULL;
+
+			if (uMessage == WM_CREATE)
+			{
+				//Set Long ptr to the class derived from base
+				CREATESTRUCT* pCreate = (CREATESTRUCT*)lParam;
+				pClass = (DERIVED*)pCreate->lpCreateParams;
+				SetWindowLongPtr(hWND, GWLP_USERDATA, (LONG_PTR)pClass);
+			}
+			else 
+			{
+				//Get the derived class
+				pClass = (DERIVED*)GetWindowLongPtr(hWND, GWLP_USERDATA);
+			}
+
+			if (pClass)
+			{
+				return pClass->MessageHandler(hWND, uMessage, wParam, lParam);
+			}
+			else
+			{
+				return DefWindowProc(hWND, uMessage, wParam, lParam);
+			}
+		}
+
+		int getX() // returbs the x position of the window
+		{
+			return x;
+		}
+
+		int getY() // returns the y position of the window
+		{
+			return y;
+		}
+
+		void setX(int x) // sets the X position of the window
+		{
+			this->x = x;
+
+			Move();
+		}
+
+		void setY(int y) // sets the Y position of the window
+		{
+			this->y = y;
+			Move();
+		}
 		
-		int getWidth();//returns the width of the window
-		int getHeight();//returns the height of the window
-		void setHeight(int newHeight); // sets the height of the window
-		void setWidth(int newWidth); // sets the width of the window
+		int getWidth()//returns the width of the window
+		{
+			return width;
+		}
 
-		void setStyles(DWORD styles);
-		DWORD getStyles();
-		void setExStyles(DWORD exStyles);
-		DWORD getExStyles();
+		int getHeight()//returns the height of the window
+		{
+			return height;
+		}
 
-		WCHAR* GetWindowName(); //returns the caption of the window frame
-		void setWindowName(WCHAR newName[]); // sets the caption of the window frame 
+		void setHeight(int newHeight) // sets the height of the window
+		{
+			height = newHeight;
+			Move();
+		}
 
-		LPVOID getLpParam(); // returns the pointer to extra data stored with window, see CREATESTRUCT
-		void setLpParam(LPVOID lpParam); // sets the pointer to new extra data
+		void setWidth(int newWidth) // sets the width of the window
+		{
+			width = newWidth;
+			Move();
+		}
 
-		HWND getHandle(); // returns the handle of the window
+		void setStyles(DWORD styles)
+		{
+			this->Styles = styles;
 
-		HWND getParentHandle(); //returns parent's window handle
-		void setParentHandle(HWND hParent); // sets window to new parent.
+			if (hWND != NULL)
+			{
+				SetWindowLongPtrW(hWND, GWL_STYLE, styles);
+				Move();//some styles require a reposition
+			}
+		}
+
+		DWORD getStyles() {
+			return Styles;
+		}
+
+		void setExStyles(DWORD exStyles) {
+			this->ExStyles = ExStyles;
+
+			if (hWND != NULL) {
+				SetWindowLongPtrW(hWND, GWL_EXSTYLE, ExStyles);
+				Move();//some styles require a reposition
+			}
+		}
+
+		DWORD getExStyles() {
+			return ExStyles;
+		}
+
+
+		WCHAR* GetWindowName() //returns the caption of the window frame
+		{
+			return WindowName;
+		}
+
+		void setWindowName(WCHAR newName[]) // sets the caption of the window frame 
+		{
+			WindowName = newName;
+
+			if (hWND != NULL)
+			{
+				SetWindowTextW(hWND, newName);
+			}
+		}
+
+		LPVOID getLpParam() // returns the pointer to extra data stored with window, see CREATESTRUCT
+		{
+			return (LPVOID)GetWindowLongPtr(hWND, GWLP_USERDATA);
+		}
+
+		void setLpParam(LPVOID lpParam) // sets the pointer to new extra data
+		{}
+
+		HWND getHandle() // returns the handle of the window
+		{
+			return hWND;
+		}
+
+		HWND getParentHandle() //returns parent's window handle
+		{
+			return hParent;
+		}
+
+		void setParentHandle(HWND hParent) // sets window to new parent.
+		{
+			this->hParent = hParent;
+
+			if (hWND != NULL)
+			{
+				SetParent(hWND, hParent);
+			}
+
+		}
+
 
 		 // intitialises class
-		Control(HINSTANCE hInstance, WCHAR ClassName[]);
+		Control(HINSTANCE hInstance, WCHAR ClassName[]) {
+			this->ClassName = ClassName;
+			this->hInstance = hInstance;
+			wc.hInstance = hInstance;
+			wc.lpszClassName = ClassName;
+			wc.lpfnWndProc = DERIVED::WindowProc;
 
-		BOOL Create(HWND hParent, HMENU hMenu, LPVOID lpParam); // creates and displays(by default) window. Returns false if procedure fails.
+			RegisterClass(&wc);
+		};
 
-		void Show(int nShowWindow);
+		BOOL Create(HWND hParent, HMENU hMenu, LPVOID lpParam) // creates and displays(by default) window. Returns false if procedure fails.
+		{
+			hWND = CreateWindowEx(
+				ExStyles,
+				ClassName,
+				WindowName,
+				Styles,
+				x, y, width, height,
+				hParent,
+				hMenu,
+				hInstance,
+				lpParam);
+
+			if (hWND != NULL)
+			{
+				Show(SW_SHOW);
+				return TRUE;
+			}
+
+			return FALSE;
+		}
+
+
+		BOOL Show(int nShowWindow)
+		{
+			return ShowWindow(hWND, nShowWindow);
+		}		
 };
 
-class Form : protected Control 
+class Form : public Control<Form> 
 {
 	
 
